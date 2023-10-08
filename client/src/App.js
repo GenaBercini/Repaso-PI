265224d6636885a84388c1d83d047a40a99a9420
Tomás Bohn Guixeras <bohnguixeras@gmail.com>
2023-10-08 12:56:43 +0200

    Creating routing and views structure

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,23 @@
-import './App.css';
+import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import { Home } from "./views/Home";
+import { NotFound } from "./views/NotFound";
+import { Landing } from "./views/Landing";
+import { RecipeDetail } from "./views/RecipeDetail";
+import { CreateRecipe } from "./views/CreateRecipe";
 
 function App() {
   return (
-    <div className="App">
-      <h1>Henry Food</h1>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Landing />} />
+        <Route path="/home" element={<Home />} />
+        <Route path="/detail/:id" element={<RecipeDetail />} />
+        <Route path="/create" element={<CreateRecipe />} />
+        <Route path="/*" element={<NotFound message="Page" />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
